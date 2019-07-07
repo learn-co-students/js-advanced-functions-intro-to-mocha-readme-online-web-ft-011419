544a8503199bad0be338860855ce7620dac3ce07
gbs4ever <gbs4ever2@gmail.com>
2019-07-07 15:58:37 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+function theTruth() {
+  return true
+}
+function sayHey() {
+  return 'Hey!'
+}
+function sayHeyFriend(name){
+  return `Hey, ${name}!`
+}
+function sayHey() {
+  return 'Hey!'
+}
+function favoriteIceCream(flavor) {
+  return `I love ${flavor}`
+}
+
+function shouting(string) {
+  return string.toUpperCase()
+}
+function roundDown(num){
+return Math.floor(num)
+}
